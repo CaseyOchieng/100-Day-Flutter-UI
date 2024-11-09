140c84bec2bc22e227ba097fe71a99aed7a7c143
Casey Ochieng <caseyochieng46@gmail.com>
2024-11-09 22:28:30 +0300

    added new images

--- flutter_day_10/lib/pages/homepage.dart
+++ flutter_day_10/lib/pages/homepage.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class Homepage extends StatelessWidget {
+class Homepage extends StatefulWidget {
   const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  final List<String> _listItem = [
+    'assets/images/five.jpg',
+    'assets/images/two.jpg',
+    'assets/images/three.jpg',
+    'assets/images/four.jpg',
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -49,6 +62,7 @@ class Homepage extends StatelessWidget {
           child: Column(
             children: [
               Container(
+                width: double.infinity,
                 height: 250,
                 decoration: const BoxDecoration(
                   borderRadius: BorderRadius.all(
@@ -68,11 +82,76 @@ class Homepage extends StatelessWidget {
                       begin: Alignment.bottomCenter,
                       colors: [
                         Colors.black.withOpacity(0.39),
-                        Colors.black.withOpacity(0.1),
+                        Colors.black.withOpacity(0.2),
                       ],
                     ),
                   ),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Text(
+                        'Lifestyle Sales',
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 35,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                      Container(
+                        height: 50,
+                        margin: const EdgeInsets.symmetric(
+                          horizontal: 40,
+                        ),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.white,
+                        ),
+                        child: Center(
+                          child: Text(
+                            'Shop Now',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(
+                              color: Colors.grey[900],
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 30,
+                      ),
+                    ],
+                  ),
                 ),
+              ),
+              const SizedBox(height: 20),
+              GridView.count(
+                crossAxisCount: 2,
+                shrinkWrap: true,
+                physics: const NeverScrollableScrollPhysics(),
+                padding: const EdgeInsets.all(20),
+                crossAxisSpacing: 20,
+                mainAxisSpacing: 20,
+                children: _listItem
+                    .map(
+                      (item) => Card(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.asset(
+                            item,
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                    )
+                    .toList(),
               ),
             ],
           ),
